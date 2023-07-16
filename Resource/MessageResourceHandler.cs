da3d2924b291b805104834eff9629e6071b87c9d
behzad dara <Behzad.dara.99@gmail.com>
2023-07-17 01:33:50 +0330

    Add project files.

---
+++ Resource/MessageResourceHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace Resource
+{
+    public static class MessageResourceHandler
+    {
+
+        private readonly static ResourceManager rm = new("Resource.Messages", typeof(MessageResourceHandler).Assembly);
+
+        public static void ChangeLanguageToPersian()
+        {
+            ChangeLanguage("fa-IR");
+        }
+
+        public static void ChangeLanguageToEnglish()
+        {
+            ChangeLanguage("en-US");
+        }
+
+        private static void ChangeLanguage(string language)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+        }
+
+        public static string Hello
+        {
+            get
+            {
+                return rm.GetString(nameof(Hello)) ?? string.Empty;
+            }
+        }
+
+    }
+
+}
---
+++ Resource/Program.cs
@@ -0,0 +1,13 @@
+﻿using Resource;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine(MessageResourceHandler.Hello);
+
+MessageResourceHandler.ChangeLanguageToPersian();
+
+Console.WriteLine(MessageResourceHandler.Hello);
+
+MessageResourceHandler.ChangeLanguageToEnglish();
+
+Console.WriteLine(MessageResourceHandler.Hello);
